278f6330a16b224e909d1c4c7238b4f1962aa501
Ailén <ailenechaguibel@gmail.com>
2025-05-30 10:58:24 +0200

    creado el servidor y conectada la bbdd funcionando ok

--- config/bbdd.js
+++ config/bbdd.js
@@ -1,1 +1,16 @@
-//Conexion a la BBDD 
+const mongoose = require('mongoose');
+require ('dotenv').config();
+
+const dbConnection = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Base de datos conectada con éxito')
+
+    } catch {
+        console.error(error);
+        throw new Error('Error en la conexión')
+
+    }
+}
+
+module.exports = { dbConnection };
--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+const { dbConnection } = require('./config/bbdd');
 
+dbConnection();
 
 app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`) );
