13b3227dab42b44bb6d7b8cf1d86284e78dbbaee
Ailén <ailenechaguibel@gmail.com>
2025-06-15 17:12:09 +0200

    marcha atras con el testing, no llegaba, pasé al README

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
-
-
-
-const app = require('./app');
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const methodOverride = require('method-override');
+const session = require('express-session');
+const swaggerUi = require('swagger-ui-express');
+const docs = require('./docs/index');
+const rutasProductos = require('./routes/rutasProductos');
+const rutasApi = require('./routes/rutasAPIproductos');
+const rutasAuth = require('./routes/authRutas');
+const carritoRuta = require('./routes/carritoRuta');
 const { dbConnection } = require('./config/bbdd');
 const PORT = process.env.PORT || 3000;
+
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(methodOverride('_method'));
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    const method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
+
+
+app.use(session({
+  secret: process.env.SESSION_SECRET, 
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use('/', rutasAuth);
+app.use('/', rutasProductos);
+app.use('/api', rutasApi);
+app.use(carritoRuta);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(docs))
+
+
+app.get('/', (req, res) => {
+  res.redirect('/dashboard');
+});
 
 
 dbConnection();
